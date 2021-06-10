a8b2e187c50be8271b4ba944ad315b9cbf4508ad
Alex Libby <alibby@sau53.org>
2021-06-10 08:17:28 -0400

    Cleaned up the json file

--- Src/index.js
+++ Src/index.js
@@ -5,46 +5,56 @@ Date.prototype.addDays = function (days) {
   return date;
 }
 
-function set_if_blank(imageUrl) {
+// Set images to Blank.jpg if no URL is given
+function set_image(imageUrl) {
 	if (imageUrl == "")
 		return "./Src/Blank.jpg";
 	return imageUrl;
 }
 
+// Set dates to 1/1/1970 if no date is given
+function set_date(date) {
+	if (date == "")
+		return "Jan 1, 1970";
+	return date;
+}
+
+// Run when the page is loaded
 window.addEventListener("load", function(event) {
+
 	// Header HTML tags
-	document.getElementById("Logo").src = set_if_blank(data["Images"]["Logo"]);
+	document.getElementById("Logo").src = set_image(data["Images"]["Logo"]);
 	document.getElementById("Title").innerHTML = data["Report"]["Title"];
 	document.getElementById("Subtitle").innerHTML = data["Report"]["Subtitle"];
-	let date = new Date(data["Invoice"]["Date"]).toLocaleDateString('en-US')
+	let date = new Date(set_date(data["Invoice"]["Date"])).toLocaleDateString('en-US')
 	document.getElementById("InvoiceDate").innerHTML += date;
-	date = new Date(data["Invoice"]["Date"]).toLocaleDateString('en-US').split("/");
+	date = new Date(set_date(data["Invoice"]["Date"])).toLocaleDateString('en-US').split("/");
 	date = date[0] + date[1] + date[2].substring(2, 4);
 	let invoiceNumber = data["Invoice"]["Number"].toString();
 	document.getElementById("InvoiceNumber").innerHTML += date + invoiceNumber;
 	document.getElementById("DeviceID").innerHTML += data["Invoice"]["DeviceID"];
 	document.getElementById("UserName").innerHTML += data["Invoice"]["UserName"];
-	
+
 	// Device info HTML tags
 	document.getElementById("DeviceBrand").innerHTML += data["Device"]["Brand"];
 	document.getElementById("DeviceSN").innerHTML += data["Device"]["Serial"];
-	date = new Date(data["Device"]["manufactured"]).toLocaleDateString('en-US')
+	date = new Date(set_date(data["Device"]["manufactured"])).toLocaleDateString('en-US')
 	document.getElementById("DeviceManuf").innerHTML += date;
 	document.getElementById("DeviceMSRP").innerHTML += "$" + data["Device"]["MSRP"];
-	
+
 	// Device image HTML tags
 	document.getElementById("DeviceLeftTitle").innerHTML = data["Images"]["Left"]["Title"];
 	document.getElementById("DeviceMiddleTitle").innerHTML = data["Images"]["Middle"]["Title"];
 	document.getElementById("DeviceRightTitle").innerHTML = data["Images"]["Right"]["Title"];
-	document.getElementById("DeviceLeftPath").src = set_if_blank(data["Images"]["Left"]["Path"]);
-	document.getElementById("DeviceMiddlePath").src = set_if_blank(data["Images"]["Middle"]["Path"]);
-	document.getElementById("DeviceRightPath").src = set_if_blank(data["Images"]["Right"]["Path"]);
-	
+	document.getElementById("DeviceLeftPath").src = set_image(data["Images"]["Left"]["Path"]);
+	document.getElementById("DeviceMiddlePath").src = set_image(data["Images"]["Middle"]["Path"]);
+	document.getElementById("DeviceRightPath").src = set_image(data["Images"]["Right"]["Path"]);
+
 	// Left side note HTML tags
 	document.getElementById("IncidentNotes").innerHTML = data["Report"]["Incident"];
 	document.getElementById("SubNoteText").innerHTML = data["Report"]["Subtitle"];
 	document.getElementById("SubNotes").innerHTML = data["Report"]["Repair"];
-	
+
 	// Itemized table HTML tags
 	document.getElementById("CostOfText").innerHTML = data["Report"]["Subtitle"];
 	let totalPrice = 0;
@@ -59,7 +69,7 @@ window.addEventListener("load", function(event) {
 		}
 		totalPrice += data["Itemized"][i]["Price"];
 	}
-	
+
 	// Cost of repair table HTML tags
 	document.getElementById("TotalPrice").innerHTML += totalPrice;
 	if(totalPrice <= data["Policy"]["MustPayMax"]) {
@@ -69,13 +79,12 @@ window.addEventListener("load", function(event) {
 		document.getElementById("TotalCoverage").innerHTML += (totalPrice-data["Policy"]["MustPayMax"]).toFixed(2);
 		document.getElementById("TotalDue").innerHTML += data["Policy"]["MustPayMax"];
 	}
-	
+
 	// Repair policy HTML tags
-	let dueDate = new Date(data["Invoice"]["Date"]);
-	dueDate = dueDate.addDays(data["Policy"]["DaysToPay"]);
+	date = new Date(set_date(data["Invoice"]["Date"]));
+	date = dueDate.addDays(data["Policy"]["DaysToPay"]);
 	document.getElementById("PayTo").innerHTML += data["Policy"]["PayableTo"];
-	document.getElementById("PayBy").innerHTML += dueDate.toLocaleDateString('en-US');
+	document.getElementById("PayBy").innerHTML += date.toLocaleDateString('en-US');
 	document.getElementById("Phone").innerHTML = data["Policy"]["Phone"];
-	
-	
+
 });
